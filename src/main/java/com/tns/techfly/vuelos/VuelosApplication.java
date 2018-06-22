de78da8c20def13ea17add25187e9d72d53b3e20
joxelito950 <jose-950@hotmail.com>
2018-06-22 14:38:57 -0500

    joxelito950
    initial proyect

---
+++ src/main/java/com/tns/techfly/vuelos/VuelosApplication.java
@@ -0,0 +1,12 @@
+package com.tns.techfly.vuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuelosApplication.class, args);
+	}
+}
