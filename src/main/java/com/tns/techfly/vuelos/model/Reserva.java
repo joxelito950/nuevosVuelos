de35581d370d6d6150efe42085fc5e2ac8c034ae
Alexamarmir21 <Alexamarmir21@techandsolve.github>
2018-06-25 14:19:32 -0500

    Alexander
    reserva

--- src/main/java/com/tns/techfly/vuelos/model/Reserva.java
+++ src/main/java/com/tns/techfly/vuelos/model/Reserva.java
@@ -1,5 +1,35 @@
 package com.tns.techfly.vuelos.model;
 
 public class Reserva {
+	private long idReserva;
+	private int cedula;
+	private int idvuelo;
+	public Reserva() {
+		super();
+	}
+	public Reserva(long idReserva, int cedula, int idvuelo) {
+		super();
+		this.idReserva = idReserva;
+		this.cedula = cedula;
+		this.idvuelo = idvuelo;
+	}
+	public long getIdReserva() {
+		return idReserva;
+	}
+	public void setIdReserva(long idReserva) {
+		this.idReserva = idReserva;
+	}
+	public int getCedula() {
+		return cedula;
+	}
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	public int getIdvuelo() {
+		return idvuelo;
+	}
+	public void setIdvuelo(int idvuelo) {
+		this.idvuelo = idvuelo;
+	}
 
 }
