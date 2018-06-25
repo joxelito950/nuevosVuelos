8a1f9d73b587eea20b92224ae47328fa58ca773b
Alexamarmir21 <Alexamarmir21@techandsolve.github>
2018-06-25 17:49:52 -0500

    Alexander
    calcularCosto

--- src/main/java/com/tns/techfly/vuelos/UtilBusiness/Business.java
+++ src/main/java/com/tns/techfly/vuelos/UtilBusiness/Business.java
@@ -3,6 +3,8 @@ package com.tns.techfly.vuelos.UtilBusiness;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.persistence.criteria.CriteriaBuilder.Case;
+
 public class Business {
 
 	public static float COST_WEEK_MORNING = 30000;
@@ -11,9 +13,26 @@ public class Business {
 	public static float COST_WEEKEND_LATE = 60000;
 
 	public static float calcularCosto(Date fecha) {
+		
 		Calendar verificar = Calendar.getInstance();
 		verificar.setTime(fecha);
+		int diaSemana = verificar.get(Calendar.DAY_OF_WEEK);
+		int Hora = verificar.get(Calendar.HOUR_OF_DAY);
+		if (diaSemana < 5) {
+			if (Hora < 12) {
+				return COST_WEEK_MORNING;
+			} else {
+				return COST_WEEK_LATE;
+			}
 
-		return COST_WEEK_LATE;
+		} else {
+			if (Hora > 12) {
+				return COST_WEEKEND_LATE;
+			} else {
+				return COST_WEEKEND_MORNING;
+			}
+
+		}
+
 	}
 }
--- src/test/java/com/tns/techfly/vuelos/UtilBusinessTest/BusinessTest.java
+++ src/test/java/com/tns/techfly/vuelos/UtilBusinessTest/BusinessTest.java
@@ -7,32 +7,34 @@ import org.junit.Test;
 
 import com.tns.techfly.vuelos.UtilBusiness.Business;
 
+import Utils.Utils;
+
 
 public class BusinessTest {
 	@Test
 	public void calculaCostoSemanaMorning() {
-		Date mañana = new Date();
+		Date mañana = Utils.obtenerFecha(4, 8); 
 		float esperado =Business.calcularCosto(mañana);
 		Assert.assertTrue(30000 == esperado);
 	}
 
 	@Test
 	public void calculaCostoSemanaLate() {
-		Date mañana = new Date();
+		Date mañana = Utils.obtenerFecha(4, 14); 
 		float esperado = Business.calcularCosto(mañana);
 		Assert.assertTrue(40000 == esperado);
 	}
 
 	@Test
 	public void calculaCostoFinSemanaLate() {
-		Date mañana = new Date();
+		Date mañana =  Utils.obtenerFecha(6, 14); 
 		float esperado = Business.calcularCosto(mañana);
 		Assert.assertTrue(60000 == esperado);
 	}
 
 	@Test
 	public void calculaCostoFinSemanaMorning() {
-		Date mañana = new Date();
+		Date mañana =  Utils.obtenerFecha(6, 4); 
 		float esperado = Business.calcularCosto(mañana);
 		Assert.assertTrue(50000 == esperado);
 	}
