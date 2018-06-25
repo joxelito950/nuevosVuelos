24ac8e7d526fa13c8df0fbbb06c8943c6e13f12b
joxelito950 <jose-950@hotmail.com>
2018-06-25 08:57:45 -0500

    joxelito950
    model

--- src/main/java/com/tns/techfly/vuelos/model/Vuelo.java
+++ src/main/java/com/tns/techfly/vuelos/model/Vuelo.java
@@ -5,10 +5,12 @@ import java.util.Date;
 public class Vuelo {
 	private int idVuelo;
 	private Date fechaSalida;
+	private float costo;
 	
 	
 	public Vuelo(int id,Date fecha) {
 		idVuelo=id;
 		fechaSalida=fecha;
+		costo=
 	}
 }
