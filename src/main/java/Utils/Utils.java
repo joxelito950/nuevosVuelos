a11419547646e572176d84ee7921f0d1809033cd
joxelito950 <jose-950@hotmail.com>
2018-06-25 15:41:15 -0500

    joxelito950
    test

--- src/main/java/Utils/Utils.java
+++ src/main/java/Utils/Utils.java
@@ -1,9 +1,19 @@
 package Utils;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Utils {
 
+	public static Date obtenerFecha(int hora) {
+		Calendar fecha=Calendar.getInstance();
+		fecha.set(Calendar.HOUR_OF_DAY, hora);
+		fecha.set(Calendar.DAY_OF_WEEK, 0);
+		Date fechaRetorna = new Date();
+		fechaRetorna=fecha.getTime();
+		return fechaRetorna;
+	}
+	
 	public static float calcularCosto(Date fecha) {
 		return 0;
 	}
--- src/test/java/UtilsTest/UtilsTest.java
+++ src/test/java/UtilsTest/UtilsTest.java
@@ -1,4 +1,4 @@
-package Utils;
+package UtilsTest;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -8,22 +8,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import Utils.Utils;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UtilsTest {
 	
 	@Test
 	public void calculaCostoMañanaSemana() {
-		Date mañana= obtenerFecha(9);
+		Date mañana= Utils.obtenerFecha(9);
 		
 	}
 	
-	public Date obtenerFecha(int hora) {
-		Calendar fecha=Calendar.getInstance();
-		fecha.set(Calendar.HOUR_OF_DAY, hora);
-		fecha.set(Calendar.DAY_OF_WEEK, 0);
-		Date fechaRetorna = new Date();
-		fechaRetorna=fecha.getTime();
-		return fechaRetorna;
-	}
+	
 }
